671000028337499d2d915cb55a3243edc682f442
Pablo Bianciotto <bianciottopablo@gmail.com>
2019-06-17 02:37:30 -0300

    Fix !translatelast failing when there is nothing to translate

--- src/matchers/translateLast.js
+++ src/matchers/translateLast.js
@@ -1,5 +1,5 @@
 const { translate } = require('../google')
-const { previousMessages } = require('../utils')
+const { previousMessages, emoji } = require('../utils')
 const { translateAndReply } = require('../translateUtils')
 
 const
@@ -16,11 +16,14 @@ exports.process = async msg => {
       last_author = prev_messages[0].author.id
       last_same_author = []
   for (let i = 0; i < prev_messages.length; i++)
-    if (prev_messages[i].author.id === last_author)
+    if (prev_messages[i].author.id === last_author && prev_messages[i].content)
       last_same_author.unshift(prev_messages[i].content)
     else
       break
-  
-  translations = await translate(last_same_author)
-  translateAndReply(msg, dm, translations)
+  if (last_same_author.length) {
+    translations = await translate(last_same_author)
+    translateAndReply(msg, dm, translations)
+  }
+  else
+    msg[dm ? 'author' : 'channel'].send(`Nothing to translate ${ emoji('pepothink') }`)
 }
