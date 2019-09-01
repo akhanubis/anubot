f6a78faf931932f9693f4e1ad548accac552aee1
Pablo Bianciotto <bianciottopablo@gmail.com>
2019-08-31 21:09:16 -0300

    Add !skip as alias for !next

--- src/matchers/music/next.js
+++ src/matchers/music/next.js
@@ -1,7 +1,7 @@
 const { stopCurrent, playNext, setLastState } = require('./musicUtils')
 
 const
-  REGEX = /^\!n(ext)?(\s+([\d.]+))?$/i,
+  REGEX = /^\!(n(ext)?|s(kip)?)(\s+([\d.]+))?$/i,
   NAME = 'Music Next'
 
 exports.name = NAME
@@ -11,7 +11,7 @@ exports.regex = REGEX
 exports.process = async msg => {
   const guild_id = setLastState(msg)
   if (guild_id) {
-    const skip_amount = parseInt(msg.content.match(REGEX)[3] || '1')
+    const skip_amount = parseInt(msg.content.match(REGEX)[5] || '1')
     stopCurrent(guild_id)
     playNext(guild_id, skip_amount)
   }
